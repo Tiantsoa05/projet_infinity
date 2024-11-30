0d880da24f2f2004b65a184d25d4a609a6db04c6
DarkZangetsu <113225391+DarkZangetsu@users.noreply.github.com>
2024-11-30 16:14:17 +0300

    Project initialisation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold mb-4 text-blue-500">Bienvenue dans React + Tailwind CSS</h1>
+        <p className="text-gray-700">Votre projet est prêt à utiliser Tailwind CSS.</p>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
